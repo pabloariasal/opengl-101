c7258cdda0d417a5ff35b28e8d44f3269868df78
Pablo Arias <pabloariasal@gmail.com>
2016-09-06 22:22:03 +0200

    Added trivial version of shaders.

---
+++ shaders/fragment_shader.glsl
@@ -0,0 +1,8 @@
+#version 403
+
+out vec4 vColor
+
+void main()
+{
+  vColor = vec4(0.0, 1.0, 0.0, 1.0);
+}
---
+++ shaders/vertex_shader.glsl
@@ -0,0 +1,9 @@
+#version 430
+
+in layout(location=0) vec2 position;
+
+void main()
+{
+   gl_Position = vec4(position, 0.0, 1.0);
+}
+
